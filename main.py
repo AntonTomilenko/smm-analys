a86b37b9f9bc65640e5e2edd275b60cd4b02ed05
whitenoize <iktomilenko@gmail.com>
2022-09-19 19:31:52 +0400

    Using functions from values_collector.py in main.py

--- main.py
+++ main.py
@@ -1,67 +1,30 @@
+from values_collector import *
+
 key_metrics = ['лайков', 'комментариев', 'репостов']
+
+# Determining the importance of metrics.
 metric_costs = []
+cicle_cost_collect(key_metrics, metric_costs)
 
-for i in key_metrics:
-    while True:
-        try:
-            cost = int(input(f'Оцените важность {i} от 1 до 10\n'))
-        except ValueError:
-            print('Похоже, вы ввели не целое число, а что-то другое — так не пойдёт')
-            continue
-
-        if float(cost) < 1 or float(cost) > 10:
-            print('Вы ввели значение, которое меньше 1 или больше 10 — оно не подойдёт. Попробуйте ещё раз')
-            continue
-        else:
-            metric_costs.append(cost)  # Convert the obtained values into convenient coefficients.
-            break
-
-
+# Determination of average values by metrics.
 metric_values = []
+cicle_value_collect(key_metrics, metric_values)
 
-for i in key_metrics:
-    while True:
-        try:
-            value = int(input(f'Введите среднее число {i} за период\n'))
-        except ValueError:
-            print('Похоже, вы ввели не целое число, а что-то другое — так не пойдёт')
-            continue
+# Accepting the number of subscribers.
+subscribers = subscribers_collect()
 
-        if float(value) < 0:
-            print('Вы ввели значение меньше 0 — оно не подойдёт. Попробуйте ещё раз')
-            continue
-        else:
-            metric_values.append(value)
-            break
-
-
-while True:
-    try:
-        subscibers = int(input('Сколько у вас подписчиков на конец исследуемого периода?\n'))
-    except ValueError:
-        print('Похоже, вы ввели не целое число, а что-то другое — так не пойдёт')
-        continue
-
-    if float(subscibers) <= 0:
-        print('Вы ввели количество подписчиков меньше 0 — так не бывает. Попробуйте ещё раз')
-        continue
-    else:
-        break
-
-
+# Convert the obtained values into convenient coefficients.
 metric_results = []
-
 for i in range(len(metric_values)):
-    # Convert the obtained values into convenient coefficients.
-    metric_results.append((int(metric_costs[i]) * int(metric_values[i])) / 10)
+    metric_results.append((metric_costs[i] * metric_values[i]) / 10)
 
-
-effective_engagement_index = (sum(metric_results) / subscibers) * 100
+# Calculation of the Effective Engagement Index (EEI).
+effective_engagement_index = (sum(metric_results) / subscribers) * 100
 
 
 print(key_metrics)
 print(metric_costs)
 print(metric_values)
-print(subscibers)
+print(subscribers)
 print(metric_results)
 print('Ваш индекс эффективного вовлечения: ' + str(effective_engagement_index) + '%')
